a5ab18df2d2e37c5e1b312ff85307d79f17f6d61
ChristianChiarulli <chris.machine@pm.me>
2021-10-31 21:07:45 -0400

    setup live kovan deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
 const fs = require("fs");
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,6 +12,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+const NODE_URL = process.env.INFURA_API_KEY;
+const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY;
 
 task("check-upkeep", "Check upkeep function", async (_, { ethers }) => {
   const addressPath = "addresses/Counter.json";
@@ -39,9 +44,17 @@ task("check-upkeep", "Check upkeep function", async (_, { ethers }) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "kovan",
+  networks: {
+    kovan: {
+      url: NODE_URL,
+      accounts: [KOVAN_PRIVATE_KEY]
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test"
+  }
 };
