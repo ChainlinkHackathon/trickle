c5733eeb3c9563219c46466bf4ca0fb846aa152c
Christian Koopmann <c.k.e.koopmann@gmail.com>
2021-11-07 13:25:32 +0100

    Move general layout into App.tsx

--- src/components/listItems.tsx
+++ src/components/listItems.tsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import AddChartIcon from '@mui/icons-material/Addchart';
 
+import AddChartIcon from '@mui/icons-material/Addchart';
 export const mainListItems = (
   <div>
     <ListItem button>
