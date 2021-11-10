d501c141e64e3f0615a1ff57032e50e2c8827cd0
Christian Koopmann <c.k.e.koopmann@gmail.com>
2021-11-10 16:20:15 +0100

    Order Allowance and Balance in rows

--- front_end/src/features/allowances/WalletBalance.tsx
+++ front_end/src/features/allowances/WalletBalance.tsx
@@ -1,27 +1,46 @@
 import { Token } from "../Main";
-import { useEthers, useTokenBalance } from "@usedapp/core";
+import { useEthers, useTokenBalance, useTokenAllowance } from "@usedapp/core";
+import networkMapping from "../../chain-info/map.json";
 import { formatUnits } from "@ethersproject/units";
 import { BalanceMsg } from "../../components";
+import { constants } from "ethers";
 
 export interface WalletBalanceProps {
-  token: Token;
+    token: Token;
 }
 
 export const WalletBalance = ({ token }: WalletBalanceProps) => {
-  const { image, address, name } = token;
+    const { image, address, name } = token;
 
-  const { account } = useEthers();
-  const tokenBalance = useTokenBalance(address, account);
+    const { account, chainId } = useEthers();
+    const tokenBalance = useTokenBalance(address, account);
+    const formattedTokenBalance: number = tokenBalance
+        ? parseFloat(formatUnits(tokenBalance, 18))
+        : 0;
 
-  const formattedTokenBalance: number = tokenBalance
-    ? parseFloat(formatUnits(tokenBalance, 18))
-    : 0;
+    const contractMapping = networkMapping[String(chainId)];
+    const spenderAddress = contractMapping?.Counter ?? constants.AddressZero;
+    const tokenAllowance = useTokenAllowance(address, account, spenderAddress);
+    const formattedTokenAllowance: number = tokenAllowance
+        ? parseFloat(formatUnits(tokenAllowance, 18))
+        : 0;
 
-  return (
-    <BalanceMsg
-      label={`Available ${name} to fund`}
-      amount={formattedTokenBalance}
-      tokenImgSrc={image}
-    />
-  );
+    return (
+        <div>
+            <div className="row">
+                <BalanceMsg
+                    label={`${name} balance in connected account`}
+                    amount={formattedTokenBalance}
+                    tokenImgSrc={image}
+                />
+            </div>
+            <div className="row">
+                <BalanceMsg
+                    label={`${name} allowance approved to be spent by contract`}
+                    amount={formattedTokenAllowance}
+                    tokenImgSrc={image}
+                />
+            </div>
+        </div>
+    );
 };
