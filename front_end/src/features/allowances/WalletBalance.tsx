cf52328ee11e0ca5684d64a7842d0148beb28823
Christian Koopmann <c.k.e.koopmann@gmail.com>
2021-11-10 18:06:27 +0100

    Add available balance to frontend

--- front_end/src/features/allowances/WalletBalance.tsx
+++ front_end/src/features/allowances/WalletBalance.tsx
@@ -1,9 +1,16 @@
 import { Token } from "../Main";
-import { useEthers, useTokenBalance, useTokenAllowance } from "@usedapp/core";
+import {
+    useEthers,
+    useTokenBalance,
+    useTokenAllowance,
+    useContractFunction,
+} from "@usedapp/core";
 import networkMapping from "../../chain-info/map.json";
 import { formatUnits } from "@ethersproject/units";
 import { BalanceMsg } from "../../components";
-import { constants } from "ethers";
+import { constants, Contract } from "ethers";
+import { Button } from "@material-ui/core";
+import ERC20 from "../../chain-info/ERC20.json";
 
 export interface WalletBalanceProps {
     token: Token;
@@ -25,6 +32,26 @@ export const WalletBalance = ({ token }: WalletBalanceProps) => {
         ? parseFloat(formatUnits(tokenAllowance, 18))
         : 0;
 
+    const availableBalance = tokenAllowance
+        ? tokenAllowance.lt(tokenBalance ?? 0)
+            ? tokenAllowance
+            : tokenBalance
+        : 0;
+    const formattedAvailableBalance: number = availableBalance
+        ? parseFloat(formatUnits(availableBalance, 18))
+        : 0;
+
+    const tokenContract = new Contract(address, ERC20.abi);
+
+    const { state, send } = useContractFunction(tokenContract, "approve", {
+        transactionName: "Approve",
+    });
+    async function approveMax() {
+        console.log("Approving Max");
+        await send(spenderAddress, constants.MaxUint256);
+        console.log("Transaction status: ", state);
+    }
+
     return (
         <div>
             <div className="row">
@@ -40,6 +67,22 @@ export const WalletBalance = ({ token }: WalletBalanceProps) => {
                     amount={formattedTokenAllowance}
                     tokenImgSrc={image}
                 />
+            </div>
+            <div className="row">
+                <BalanceMsg
+                    label={`${name} balance available for DCA`}
+                    amount={formattedAvailableBalance}
+                    tokenImgSrc={image}
+                />
+            </div>
+            <div className="row">
+                <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={() => approveMax()}
+                >
+                    Approve Maximum Allowance
+                </Button>
             </div>
         </div>
     );
