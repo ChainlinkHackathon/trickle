88035b5db3a699d4e5c721f49394d05ca0efc6bf
Christian Koopmann <c.k.e.koopmann@gmail.com>
2021-11-10 10:28:35 +0100

    Adjust deploy script to save address and abi in desired place

--- contracts/TokenAllowances.sol
+++ contracts/TokenAllowances.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenAllowances {
-    function getAllowance(IERC20 token) external view (uint256 allowance) {
-        return token.allowance(msg.sender, this)
+    function getAllowance(IERC20 token) external view returns(uint256) {
+        return token.allowance(msg.sender, address(this));
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,61 +1,59 @@
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config()
+require("dotenv").config();
 const fs = require("fs");
-
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY;
 const KOVAN_JSON_RPC_URL = process.env.KOVAN_JSON_RPC_URL;
-console.log(NODE_URL)
 
 task("check-upkeep", "Check upkeep function", async (_, { ethers }) => {
-  const addressPath = "addresses/Counter.json";
-  const network = await ethers.provider.getNetwork();
-  const chainId = network.chainId;
-  console.log("Checking upkeep on chain: ", chainId);
-  const addresses = JSON.parse(fs.readFileSync(addressPath, "utf8"));
-  const address = addresses[chainId];
+    const addressPath = "addresses/Counter.json";
+    const network = await ethers.provider.getNetwork();
+    const chainId = network.chainId;
+    console.log("Checking upkeep on chain: ", chainId);
+    const addresses = JSON.parse(fs.readFileSync(addressPath, "utf8"));
+    const address = addresses[chainId];
 
-  const artifactPath = "artifacts/contracts/Counter.sol/Counter.json";
-  const contractArtifact = JSON.parse(fs.readFileSync(artifactPath, "utf8"));
-  const abi = contractArtifact.abi;
+    const artifactPath = "artifacts/contracts/Counter.sol/Counter.json";
+    const contractArtifact = JSON.parse(fs.readFileSync(artifactPath, "utf8"));
+    const abi = contractArtifact.abi;
 
-  const keeperContract = await ethers.getContractAt(abi, address);
-  await keeperContract.deployed()
+    const keeperContract = await ethers.getContractAt(abi, address);
+    await keeperContract.deployed();
 
-  console.log("Connected to contract instance: ", keeperContract)
+    console.log("Connected to contract instance: ", keeperContract);
 
-  const callData = ethers.utils.formatBytes32String("")
-  console.log("Call Data", callData)
+    const callData = ethers.utils.formatBytes32String("");
+    console.log("Call Data", callData);
 
-  const [upkeepNeeded, performData] = await keeperContract.checkUpkeep(callData);
-  console.log("The status of this upkeep is currently", upkeepNeeded);
-  console.log("Here is the perform data", performData);
+    const [upkeepNeeded, performData] = await keeperContract.checkUpkeep(callData);
+    console.log("The status of this upkeep is currently", upkeepNeeded);
+    console.log("Here is the perform data", performData);
 });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 module.exports = {
-  solidity: "0.8.4",
-  defaultNetwork: "kovan",
-  networks: {
-    kovan: {
-      url: `${KOVAN_JSON_RPC_URL}`,
-      accounts: [`${KOVAN_PRIVATE_KEY}`]
-    }
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test"
-  }
+    solidity: "0.8.4",
+    defaultNetwork: "kovan",
+    networks: {
+        kovan: {
+            url: `${KOVAN_JSON_RPC_URL}`,
+            accounts: [`${KOVAN_PRIVATE_KEY}`],
+        },
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+    },
 };
