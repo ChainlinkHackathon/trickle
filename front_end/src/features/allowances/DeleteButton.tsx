5a457e12507a73bd9f328502b2684c102a26de4e
Christian Koopmann <c.k.e.koopmann@gmail.com>
2021-11-28 11:37:49 +0100

    Add loading spinner to delete button

--- front_end/src/features/allowances/DeleteButton.tsx
+++ front_end/src/features/allowances/DeleteButton.tsx
@@ -1,19 +1,21 @@
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { useTrickle } from "../../hooks";
+import { CircularProgress } from "@material-ui/core";
 
 export function DeleteButton(params: any) {
-
-    const { deleteOrderSend } = useTrickle();
+    const { deleteOrderSend, deleteOrderState } = useTrickle();
+    const isMining = deleteOrderState.status === "Mining";
     async function handleClick() {
-        const tokenPairHash = params.getValue(params.id, "tokenPairHash")
-        const orderHash = params.getValue(params.id, "orderHash")
+        const tokenPairHash = params.getValue(params.id, "tokenPairHash");
+        const orderHash = params.getValue(params.id, "orderHash");
         console.log("Deleting", tokenPairHash, orderHash);
         await deleteOrderSend(tokenPairHash, orderHash);
         console.log("Deleted");
     }
 
-
     return (
-        <Button variant="outlined" color="error" onClick={handleClick}>Delete</Button>
+        <Button variant="outlined" color="error" onClick={handleClick}>
+            {isMining ? <CircularProgress size={26} /> : "Delete"}
+        </Button>
     );
 }
