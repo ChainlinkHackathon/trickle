6389bde541ea3e906a119a4db0b87370d29b6999
ChristianChiarulli <chris.machine@pm.me>
2021-11-30 14:38:10 -0500

    fix: UI tweaks for buttons and existing orders grid border

--- front_end/src/features/allowances/DcaForm.tsx
+++ front_end/src/features/allowances/DcaForm.tsx
@@ -1,289 +1,279 @@
-import { useEffect, useState } from "react";
-import { Token } from "../Main";
+import { useEffect, useState } from 'react'
+import { Token } from '../Main'
 import {
-    useEthers,
-    useNotifications,
-    useContractFunction,
-    useTokenAllowance,
-} from "@usedapp/core";
+  useEthers,
+  useNotifications,
+  useContractFunction,
+  useTokenAllowance,
+} from '@usedapp/core'
 import {
-    Button,
-    CircularProgress,
-    Snackbar,
-    TextField,
-    Select,
-    MenuItem,
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core";
-import { useTrickle } from "../../hooks";
-import { constants, utils, Contract } from "ethers";
-import Alert from "@material-ui/lab/Alert";
-import FormHelperText from "@mui/material/FormHelperText";
-import FormControl from "@mui/material/FormControl";
-import ERC20 from "../../chain-info/ERC20.json";
+  Button,
+  CircularProgress,
+  Snackbar,
+  TextField,
+  Select,
+  MenuItem,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
+import { useTrickle } from '../../hooks'
+import { constants, utils, Contract } from 'ethers'
+import Alert from '@material-ui/lab/Alert'
+import FormHelperText from '@mui/material/FormHelperText'
+import FormControl from '@mui/material/FormControl'
+import ERC20 from '../../chain-info/ERC20.json'
 
 // This is the typescript way of saying this compent needs this type
 export interface DcaFormProps {
-    supportedTokens: Array<Token>;
+  supportedTokens: Array<Token>
 }
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: theme.spacing(2),
-        width: "100%",
-    },
-    boxWrapper: {
-        display: "flex",
-        // flexDirection: 'column',
-        alignItems: "center",
-        justifyContent: "space-around",
-        gap: theme.spacing(2),
-        width: "100%",
-    },
-    box: {
-        backgroundColor: "rgba(0, 0, 0, 0.06)",
-        borderRadius: "25px",
-        width: "50%",
-        display: "flex",
-        padding: "20px",
-        // justifyContent: 'center',
-        // alignContent: 'center'
-    },
-    slider: {
-        width: "100%",
-        maxWidth: "400px",
-        padding: "10px 26px",
-    },
-}));
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: theme.spacing(2),
+    width: '100%',
+    padding: '0 0 28px 0',
+  },
+  boxWrapper: {
+    display: 'flex',
+    // flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    gap: theme.spacing(2),
+    width: '100%',
+  },
+  tokenWrapper: {
+    margin: '10px 0px 0px 0px',
+  },
+  box: {
+    backgroundColor: 'rgba(0, 0, 0, 0.06)',
+    borderRadius: '25px',
+    width: '50%',
+    display: 'flex',
+    padding: '20px',
+    // justifyContent: 'center',
+    // alignContent: 'center'
+  },
+  slider: {
+    width: '100%',
+    maxWidth: '400px',
+    padding: '10px 26px',
+  },
+}))
 
 // token is getting passed in as a prop
 // in the ping brackets is an object/variable
 // That object is of the shape DcaFormProps
 export const DcaForm = ({ supportedTokens }: DcaFormProps) => {
-    const { account } = useEthers();
-    const { notifications } = useNotifications();
+  const { account } = useEthers()
+  const { notifications } = useNotifications()
 
-    const classes = useStyles();
+  const classes = useStyles()
 
-    const { setDcaSend, setDcaState, trickleContractAddress } = useTrickle();
+  const { setDcaSend, setDcaState, trickleContractAddress } = useTrickle()
 
-    const handleDcaSubmit = () => {
-        const amountAsWei = utils.parseEther(amount.toString());
-        return setDcaSend(
-            sellToken,
-            buyToken,
-            amountAsWei,
-            interval.toString()
-        );
-    };
+  const handleDcaSubmit = () => {
+    const amountAsWei = utils.parseEther(amount.toString())
+    return setDcaSend(sellToken, buyToken, amountAsWei, interval.toString())
+  }
 
-    const [sellToken, setSellToken] = useState<string>(
-        "0xad5ce863ae3e4e9394ab43d4ba0d80f419f61789"
-    );
-    const handleSellTokenChange = (event: any) => {
-        setSellToken(event.target.value);
-    };
+  const [sellToken, setSellToken] = useState<string>(
+    '0xad5ce863ae3e4e9394ab43d4ba0d80f419f61789'
+  )
+  const handleSellTokenChange = (event: any) => {
+    setSellToken(event.target.value)
+  }
 
-    const tokenContract = new Contract(sellToken, ERC20.abi);
+  const tokenContract = new Contract(sellToken, ERC20.abi)
 
-    const tokenAllowance = useTokenAllowance(
-        sellToken,
-        account,
-        trickleContractAddress
-    );
+  const tokenAllowance = useTokenAllowance(
+    sellToken,
+    account,
+    trickleContractAddress
+  )
 
-    const { state: approveState, send: approveSend } = useContractFunction(
-        tokenContract,
-        "approve",
-        {
-            transactionName: "Approve",
-        }
-    );
-    const isMaxApproved = tokenAllowance
-        ? tokenAllowance.eq(constants.MaxUint256)
-        : false;
+  const { state: approveState, send: approveSend } = useContractFunction(
+    tokenContract,
+    'approve',
+    {
+      transactionName: 'Approve',
+    }
+  )
+  const isMaxApproved = tokenAllowance
+    ? tokenAllowance.eq(constants.MaxUint256)
+    : false
 
-    const isApprovalMining = approveState.status === "Mining";
+  const isApprovalMining = approveState.status === 'Mining'
 
-    async function approveMax() {
-        await approveSend(trickleContractAddress, constants.MaxUint256);
+  async function approveMax() {
+    await approveSend(trickleContractAddress, constants.MaxUint256)
+  }
+
+  const [buyToken, setBuyToken] = useState<string>(
+    '0xad5ce863ae3e4e9394ab43d4ba0d80f419f61789'
+  )
+  const handleBuyTokenChange = (event: any) => {
+    setBuyToken(event.target.value)
+  }
+
+  const [amount, setAmount] = useState<number>(0)
+  const handleAmountChange = (event: any) => {
+    setAmount(event.target.value)
+  }
+
+  const [interval, setInterval] = useState<
+    number | string | Array<number | string>
+  >(0)
+  const handleIntervalChange = (event: any) => {
+    setInterval(event.target.value)
+  }
+
+  const [showErc20ApprovalSuccess, setShowErc20ApprovalSuccess] =
+    useState(false)
+  const [showDcaTokensSuccess, setshowDcaTokensSuccess] = useState(false)
+
+  const handleCloseSnack = () => {
+    showErc20ApprovalSuccess && setShowErc20ApprovalSuccess(false)
+    showDcaTokensSuccess && setshowDcaTokensSuccess(false)
+  }
+
+  useEffect(() => {
+    if (
+      notifications.filter(
+        (notification) =>
+          notification.type === 'transactionSucceed' &&
+          notification.transactionName === 'Approve ERC20 transfer'
+      ).length > 0
+    ) {
+      !showErc20ApprovalSuccess && setShowErc20ApprovalSuccess(true)
+      showDcaTokensSuccess && setshowDcaTokensSuccess(false)
     }
 
-    const [buyToken, setBuyToken] = useState<string>(
-        "0xad5ce863ae3e4e9394ab43d4ba0d80f419f61789"
-    );
-    const handleBuyTokenChange = (event: any) => {
-        setBuyToken(event.target.value);
-    };
+    if (
+      notifications.filter(
+        (notification) =>
+          notification.type === 'transactionSucceed' &&
+          notification.transactionName === 'DCA tokens'
+      ).length > 0
+    ) {
+      showErc20ApprovalSuccess && setShowErc20ApprovalSuccess(false)
+      !showDcaTokensSuccess && setshowDcaTokensSuccess(true)
+    }
+  }, [notifications, showErc20ApprovalSuccess, showDcaTokensSuccess])
 
-    const [amount, setAmount] = useState<number>(0);
-    const handleAmountChange = (event: any) => {
-        setAmount(event.target.value);
-    };
+  const isMining = setDcaState.status === 'Mining'
 
-    const [interval, setInterval] = useState<
-        number | string | Array<number | string>
-    >(0);
-    const handleIntervalChange = (event: any) => {
-        setInterval(event.target.value);
-    };
+  const hasZeroAmountSelected = parseFloat(amount.toString()) === 0
 
-    const [showErc20ApprovalSuccess, setShowErc20ApprovalSuccess] =
-        useState(false);
-    const [showDcaTokensSuccess, setshowDcaTokensSuccess] = useState(false);
-
-    const handleCloseSnack = () => {
-        showErc20ApprovalSuccess && setShowErc20ApprovalSuccess(false);
-        showDcaTokensSuccess && setshowDcaTokensSuccess(false);
-    };
-
-    useEffect(() => {
-        if (
-            notifications.filter(
-                (notification) =>
-                    notification.type === "transactionSucceed" &&
-                    notification.transactionName === "Approve ERC20 transfer"
-            ).length > 0
-        ) {
-            !showErc20ApprovalSuccess && setShowErc20ApprovalSuccess(true);
-            showDcaTokensSuccess && setshowDcaTokensSuccess(false);
-        }
-
-        if (
-            notifications.filter(
-                (notification) =>
-                    notification.type === "transactionSucceed" &&
-                    notification.transactionName === "DCA tokens"
-            ).length > 0
-        ) {
-            showErc20ApprovalSuccess && setShowErc20ApprovalSuccess(false);
-            !showDcaTokensSuccess && setshowDcaTokensSuccess(true);
-        }
-    }, [notifications, showErc20ApprovalSuccess, showDcaTokensSuccess]);
-
-    const isMining = setDcaState.status === "Mining";
-
-    const hasZeroAmountSelected = parseFloat(amount.toString()) === 0;
-
-    return (
-        <>
-            <div className={classes.boxWrapper}>
-                <div className="row">
-                    <FormControl sx={{ m: 1, minWidth: 120 }}>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={sellToken}
-                            label="Sell Token"
-                            onChange={handleSellTokenChange}
-                        >
-                            {supportedTokens.map((token) => {
-                                return (
-                                    <MenuItem value={token.address}>
-                                        {token.name}
-                                    </MenuItem>
-                                );
-                            })}
-                        </Select>
-                        <FormHelperText>Token to spend</FormHelperText>
-                    </FormControl>
-                    {!isMaxApproved ? (
-                        <Button
-                            color="primary"
-                            variant="contained"
-                            onClick={() => approveMax()}
-                        >
-                            {isApprovalMining ? (
-                                <CircularProgress size={26} />
-                            ) : (
-                                "Approve"
-                            )}
-                        </Button>
-                    ) : (
-                        ""
-                    )}
-                </div>
-            </div>
-            <div className={classes.boxWrapper}>
-                <TextField
-                    id={`buyToken-input`}
-                    className={classes.slider}
-                    value={buyToken}
-                    margin="dense"
-                    onChange={handleBuyTokenChange}
-                    helperText="Address of Token to Buy"
-                    inputProps={{
-                        type: "string",
-                    }}
-                />
-            </div>
-            <div className={classes.boxWrapper}>
-                <TextField
-                    id={`interval-input`}
-                    className={classes.slider}
-                    value={amount}
-                    onChange={handleAmountChange}
-                    disabled={isMining}
-                    inputProps={{
-                        step: 0.1,
-                        min: 0,
-                        type: "number",
-                    }}
-                    helperText={`Amount of sell token to spend`}
-                />
-            </div>
-            <div className={classes.boxWrapper}>
-                <TextField
-                    id={`interval-input`}
-                    className={classes.slider}
-                    value={interval}
-                    onChange={handleIntervalChange}
-                    disabled={isMining}
-                    inputProps={{
-                        step: 1,
-                        min: 0,
-                        type: "number",
-                    }}
-                    helperText="Execution Interval in seconds"
-                />
-            </div>
-            <div className={classes.container}>
-                <Button
-                    color="primary"
-                    variant="contained"
-                    size="large"
-                    onClick={handleDcaSubmit}
-                    disabled={isMining || hasZeroAmountSelected}
-                >
-                    {isMining ? (
-                        <CircularProgress size={26} />
-                    ) : (
-                        "Set Recurring Purchase"
-                    )}
-                </Button>
-            </div>
-            <Snackbar
-                open={showErc20ApprovalSuccess}
-                autoHideDuration={5000}
-                onClose={handleCloseSnack}
+  return (
+    <>
+      <div className={classes.boxWrapper}>
+        <div className='row'>
+          <FormControl
+            sx={{ m: 2, minWidth: 120 }}
+          >
+            <Select
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              value={sellToken}
+              label='Sell Token'
+              onChange={handleSellTokenChange}
             >
-                <Alert onClose={handleCloseSnack} severity="success">
-                    ERC-20 token transfer approved successfully! Now approve the
-                    2nd tx to initiate the staking transfer.
-                </Alert>
-            </Snackbar>
-            <Snackbar
-                open={showDcaTokensSuccess}
-                autoHideDuration={5000}
-                onClose={handleCloseSnack}
+              {supportedTokens.map((token) => {
+                return <MenuItem value={token.address}>{token.name}</MenuItem>
+              })}
+            </Select>
+            <FormHelperText>Token to spend</FormHelperText>
+          </FormControl>
+          {!isMaxApproved ? (
+            <Button
+              className={classes.tokenWrapper}
+              color='primary'
+              variant='contained'
+              onClick={() => approveMax()}
             >
-                <Alert onClose={handleCloseSnack} severity="success">
-                    Recurring buy set successfully!
-                </Alert>
-            </Snackbar>
-        </>
-    );
-};
+              {isApprovalMining ? <CircularProgress size={26} /> : 'Approve'}
+            </Button>
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
+      <div className={classes.boxWrapper}>
+        <TextField
+          id={`buyToken-input`}
+          className={classes.slider}
+          value={buyToken}
+          margin='dense'
+          onChange={handleBuyTokenChange}
+          helperText='Address of Token to Buy'
+          inputProps={{
+            type: 'string',
+          }}
+        />
+      </div>
+      <div className={classes.boxWrapper}>
+        <TextField
+          id={`interval-input`}
+          className={classes.slider}
+          value={amount}
+          onChange={handleAmountChange}
+          disabled={isMining}
+          inputProps={{
+            step: 0.1,
+            min: 0,
+            type: 'number',
+          }}
+          helperText={`Amount of sell token to spend`}
+        />
+      </div>
+      <div className={classes.boxWrapper}>
+        <TextField
+          id={`interval-input`}
+          className={classes.slider}
+          value={interval}
+          onChange={handleIntervalChange}
+          disabled={isMining}
+          inputProps={{
+            step: 1,
+            min: 0,
+            type: 'number',
+          }}
+          helperText='Execution Interval in seconds'
+        />
+      </div>
+      <div className={classes.container}>
+        <Button
+          color='primary'
+          variant='contained'
+          size='large'
+          onClick={handleDcaSubmit}
+          disabled={isMining || hasZeroAmountSelected}
+        >
+          {isMining ? <CircularProgress size={26} /> : 'Set Recurring Purchase'}
+        </Button>
+      </div>
+      <Snackbar
+        open={showErc20ApprovalSuccess}
+        autoHideDuration={5000}
+        onClose={handleCloseSnack}
+      >
+        <Alert onClose={handleCloseSnack} severity='success'>
+          ERC-20 token transfer approved successfully! Now approve the 2nd tx to
+          initiate the staking transfer.
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={showDcaTokensSuccess}
+        autoHideDuration={5000}
+        onClose={handleCloseSnack}
+      >
+        <Alert onClose={handleCloseSnack} severity='success'>
+          Recurring buy set successfully!
+        </Alert>
+      </Snackbar>
+    </>
+  )
+}
--- front_end/src/features/allowances/OrderTable.tsx
+++ front_end/src/features/allowances/OrderTable.tsx
@@ -1,94 +1,101 @@
-import { DataGrid } from "@mui/x-data-grid";
-import { useTrickle } from "../../hooks";
-import { useEffect, useMemo } from "react";
-import { utils } from "ethers";
-import { DeleteButton } from "./DeleteButton";
+import { DataGrid } from '@mui/x-data-grid'
+import { useTrickle } from '../../hooks'
+import { useEffect, useMemo } from 'react'
+import { utils } from 'ethers'
+import { DeleteButton } from './DeleteButton'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  dataGrid: {
+    borderRadius: '25px !important',
+  },
+}))
 
 const columns = [
-    { field: "sellToken", headerName: "Sell Token", width: 150 },
-    { field: "buyToken", headerName: "Buy Token", width: 150 },
-    {
-        field: "sellAmountFormatted",
-        headerName: "Amount",
-        type: "number",
-        width: 90,
-        valueGetter: (params: any) =>
-            utils.formatEther(params.getValue(params.id, "sellAmount")),
+  { field: 'sellToken', headerName: 'Sell Token', width: 150 },
+  { field: 'buyToken', headerName: 'Buy Token', width: 150 },
+  {
+    field: 'sellAmountFormatted',
+    headerName: 'Amount',
+    type: 'number',
+    width: 90,
+    valueGetter: (params: any) =>
+      utils.formatEther(params.getValue(params.id, 'sellAmount')),
+  },
+  {
+    field: 'interval',
+    headerName: 'interval',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'lastExecutionFormatted',
+    headerName: 'Last Execution',
+    type: 'string',
+    width: 150,
+    valueGetter: (params: any) => {
+      const rawValue = params.getValue(params.id, 'lastExecution').toNumber()
+      if (rawValue === 0) {
+        return 'NONE'
+      } else {
+        const date = new Date(rawValue * 1000)
+        return date.toLocaleString()
+      }
     },
-    {
-        field: "interval",
-        headerName: "interval",
-        type: "number",
-        width: 90,
+  },
+  {
+    field: 'nextExecution',
+    headerName: 'Next Execution',
+    type: 'string',
+    width: 150,
+    valueGetter: (params: any) => {
+      const rawValue = params
+        .getValue(params.id, 'lastExecution')
+        .add(params.getValue(params.id, 'interval'))
+        .toNumber()
+      const date = new Date(rawValue * 1000)
+      const now = new Date()
+      if (date < now) {
+        return 'NOW'
+      } else {
+        return date.toLocaleString()
+      }
     },
-    {
-        field: "lastExecutionFormatted",
-        headerName: "Last Execution",
-        type: "string",
-        width: 150,
-        valueGetter: (params: any) => {
-            const rawValue = params
-                .getValue(params.id, "lastExecution")
-                .toNumber();
-            if (rawValue === 0) {
-                return "NONE";
-            } else {
-                const date = new Date(rawValue * 1000);
-                return date.toLocaleString();
-            }
-        },
-    },
-    {
-        field: "nextExecution",
-        headerName: "Next Execution",
-        type: "string",
-        width: 150,
-        valueGetter: (params: any) => {
-            const rawValue = params
-                .getValue(params.id, "lastExecution")
-                .add(params.getValue(params.id, "interval"))
-                .toNumber();
-            const date = new Date(rawValue * 1000);
-            const now = new Date();
-            if (date < now) {
-                return "NOW";
-            } else {
-                return date.toLocaleString();
-            }
-        },
-    },
-    {
-        field: "delete",
-        headerName: "Delete",
-        sortable: false,
-        renderCell: DeleteButton,
-    },
-];
+  },
+  {
+    field: 'delete',
+    headerName: 'Delete',
+    sortable: false,
+    renderCell: DeleteButton,
+  },
+]
 
 export function OrderTable() {
-    const { orders } = useTrickle();
-    const rows = useMemo(
-        () =>
-            orders
-                ? orders[0].map((order: any, i: number) => {
-                      return { id: i, ...order };
-                  })
-                : [],
-        [orders]
-    );
+  const classes = useStyles()
+  const { orders } = useTrickle()
+  const rows = useMemo(
+    () =>
+      orders
+        ? orders[0].map((order: any, i: number) => {
+            return { id: i, ...order }
+          })
+        : [],
+    [orders]
+  )
 
-    useEffect(() => {
-        console.log("rows", rows);
-    }, [rows]);
+  useEffect(() => {
+    console.log('rows', rows)
+  }, [rows])
 
-    return (
-        <div style={{ height: 400, width: "100%" }}>
-            <DataGrid
-                rows={rows}
-                columns={columns as any}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-            />
-        </div>
-    );
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        className={classes.dataGrid}
+        rows={rows}
+        columns={columns as any}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+      />
+    </div>
+  )
 }
